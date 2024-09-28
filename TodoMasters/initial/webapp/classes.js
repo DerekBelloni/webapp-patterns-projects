5322121015ae4bcd2a1494fcdaef2a49e8d6c578
Derek Belloni <derekbelloni10@gmail.com>
2024-09-28 11:21:18 -0600

    implemented basic list behavior for the todo list class

--- TodoMasters/initial/webapp/classes.js
+++ TodoMasters/initial/webapp/classes.js
@@ -1,4 +1,4 @@
-import { observerMixin } from "./mixin";
+import { observerMixin } from "./mixin.js";
 
 class TodoItem {
     constructor(text) {
@@ -16,6 +16,11 @@ class TodoList { // candidate for Singleton, only want one todo list
     
 
     // Singleton
+    constructor() {
+        if (TodoList.instance) {
+            throw new Error("Use TodoList.getInstance() to access the list");
+        }
+    } 
     static instance = null;
     static {
         this.instance = new TodoList();
@@ -23,11 +28,33 @@ class TodoList { // candidate for Singleton, only want one todo list
     static getInstance() {
         return this.instance;
     }
-    constructor() {
-        if (TodoList.instance) {
-            throw new Error("Use TodoList.getInstance() to access the list");
+
+
+    // List Behavior
+    add(item) {
+        const array = Array.from(this.#data);
+        const todoExists = array.filter(t => t.equals(item)).length > 0;
+        if (!todoExists) {
+            this.#data.add(item);
+            this.notify();
         }
-    } 
+    }
+    delete(todo_text) {
+        const array = Array.from(this.#data);
+        // TODO: Check for errors
+        const todoToDelete = array.filter(t => t.text == todo_text)[0];
+        this.#data.delete(todoToDelete);
+        this.notify();
+    }
+    find (todo_text) {
+        const array = Array.from(this.#data);
+        return array.find(t => t.text == todo_text)
+    }
+    replaceList(list) {
+        this.#data = list;
+        this.notify();
+    }
 }
 
+// Applying the observer mixin to the class
 Object.assign(TodoList.prototype, observerMixin)
